6bd52280596aea1f39fae58fa95e543706aac567
Abdelmoutaleb EL BOUAZZATI <a.elbouazzati@laposte.net>
2017-05-22 14:59:50 +0200

    tp03

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,27 +16,13 @@ protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
 
 ServletException, IOException {
 
-// récupère la valeur d'un paramètre dont le nom est avecPhoto
+// A ajouter
 
-String avecPhotoParam = req.getParameter("avecPhoto");
+req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
 
-// récupère la valeur d'un paramètre dont le nom est departement
+req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp")
 
-String departementParam = req.getParameter("departement");
-
-resp.setContentType("text/html");
-
-// code HTML écrit dans le corps de la réponse
-
-resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-
-+ "<ul>"
-
-+ "<li>avecPhoto="+ avecPhotoParam + "</li>"
-
-+ "<li>departement="+ departementParam + "</li>"
-
-+ "</ul>");
+.forward(req, resp);
 
 }
 
